48ff9dea6c6d5ba688211c88fa2d97e209037a00
TianqGuo <tianquanguo@gmail.com>
2023-06-21 22:11:16 -0700

    Added pickle related unpickler and the code.

--- IntelliHealthApp/src/main/java/com/intellihealthapp/main/controller/ChatController.java
+++ IntelliHealthApp/src/main/java/com/intellihealthapp/main/controller/ChatController.java
@@ -3,14 +3,19 @@ package com.intellihealthapp.main.controller;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.intellihealthapp.main.service.ChatService;
 import lombok.extern.slf4j.Slf4j;
+import net.razorvine.pickle.PickleException;
+import net.razorvine.pickle.Unpickler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
 import java.util.List;
+import java.util.Map;
 
 @Slf4j
 @Controller
@@ -32,6 +37,27 @@ public class ChatController {
     @RequestMapping("/test")
     @ResponseBody
     public String dummyMessage() {
+        InputStream fis = null;
+        try {
+            fis = new FileInputStream("src/main/java/com/intellihealthapp/main/utils/model.pkl");
+            System.out.println(fis);
+            Unpickler unpickler = new Unpickler();
+//            Object model = unpickler.load(fis);
+            Map<String, Object> model = (Map<String, Object>)unpickler.load(fis);
+            System.out.println(model);
+        } catch (IOException | PickleException e) {
+            // Handle exceptions
+            e.printStackTrace();
+        } finally {
+            if (fis != null) {
+                try {
+                    fis.close();
+                } catch (IOException e) {
+                    // Handle exceptions
+                    e.printStackTrace();
+                }
+            }
+        }
         return "hello world";
     }
 }
