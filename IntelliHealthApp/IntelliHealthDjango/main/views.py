1e6b9f67a49cc29a35a8f7376acffba4ce73a8cc
TianqGuo <tianquanguo@gmail.com>
2023-06-26 23:18:56 -0700

    Link to chatgpt is successful

--- IntelliHealthApp/IntelliHealthDjango/main/views.py
+++ IntelliHealthApp/IntelliHealthDjango/main/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+
+from utils.utils import Client
+
 
 def api_health(request):
     return HttpResponse("{api_health: \"Success\", version_number: 99}")
 
 
 def chatgpt(request):
-    return HttpResponse("another test")
+    cur_client = Client()
+    text = cur_client.ask_question("How to efficiently learn ai technology?")
+    print(text)
+    return JsonResponse(text)
