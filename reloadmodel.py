030057ad276dae70856f0b0e4765e40d6be088d1
kevin866 <k.wang1502483@gmail.com>
2023-06-27 06:52:15 +0000

    update

--- reloadmodel.py
+++ reloadmodel.py
@@ -2,5 +2,6 @@ from keras.backend import dropout
 import joblib
 import pandas as pd
 import numpy as np
-model = joblib.load('model.pkl')
-model.save('my_model')
+import tensorflow as tf
+new_model = tf.keras.models.load_model('saved_model/my_model')
+
