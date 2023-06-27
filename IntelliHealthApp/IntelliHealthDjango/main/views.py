ba2b44db0a479e26e64408a96da3998724547eb7
TianqGuo <tianquanguo@gmail.com>
2023-06-26 22:23:48 -0700

    Added 2 additional pages, /api and /api/chat

--- IntelliHealthApp/IntelliHealthDjango/main/views.py
+++ IntelliHealthApp/IntelliHealthDjango/main/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
+
+def api_health(request):
+    return HttpResponse("{api_health: \"Success\", version_number: 99}")
+
+
+def chatgpt(request):
+    return HttpResponse("another test")
