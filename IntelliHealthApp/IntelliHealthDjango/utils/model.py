76c187dd216c3d24b41dd130031968c60c790908
TianqGuo <tianquanguo@gmail.com>
2023-06-27 22:13:13 -0700

    Added model related loading but there is an issue that model cannot be load successfully.

--- IntelliHealthApp/IntelliHealthDjango/utils/model.py
+++ IntelliHealthApp/IntelliHealthDjango/utils/model.py
@@ -6,7 +6,7 @@ import tensorflow as tf
 def test():
     model = tf.keras.models.load_model('my_model.h5')
     config = model.get_config() # Returns pretty much every information about your model
-    # print(config)
+    print(config)
     # print(config["layers"][0]["config"]["batch_input_shape"]) # returns a tuple of width, height and channels
     df = pd.read_csv('preprocessed_kevin')
     y = ['metformin', 'repaglinide','nateglinide', 'chlorpropamide', 'glimepiride', 'acetohexamide',
--- IntelliHealthApp/IntelliHealthSpring/src/main/java/com/intellihealthapp/main/IntelliHealthAppApplicationConfig.java
+++ IntelliHealthApp/IntelliHealthSpring/src/main/java/com/intellihealthapp/main/IntelliHealthAppApplicationConfig.java
@@ -1,9 +1,14 @@
 package com.intellihealthapp.main;
 
+import com.intellihealthapp.main.utils.ModelGenerator;
 import com.intellihealthapp.main.utils.RequestsGenerator;
+import org.deeplearning4j.nn.modelimport.keras.exceptions.InvalidKerasConfigurationException;
+import org.deeplearning4j.nn.modelimport.keras.exceptions.UnsupportedKerasConfigurationException;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import java.io.IOException;
 
 @Configuration
 @EnableWebMvc
@@ -12,5 +17,11 @@ public class IntelliHealthAppApplicationConfig {
     public RequestsGenerator newGenerator() {
         RequestsGenerator generator = new RequestsGenerator();
         return generator;
+    }
+
+    @Bean(name = "model")
+    public ModelGenerator newModelGenerator() throws IOException, UnsupportedKerasConfigurationException, InvalidKerasConfigurationException {
+        ModelGenerator model = new ModelGenerator();
+        return model;
     }
 }
