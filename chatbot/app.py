68a67da2aad0880191d1dda934289fc6af61e44d
Justin Zhang <dlyzxl@yahoo.com>
2023-07-08 11:44:35 -0500

    update chatbot

--- chatbot/app.py
+++ chatbot/app.py
@@ -1,12 +1,187 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
+from dotenv import load_dotenv
+import pdb
+import os
+import openai
+import pinecone
+from dotenv import dotenv_values
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.vectorstores import Pinecone
+from langchain.document_loaders import TextLoader
+from langchain.chat_models import ChatOpenAI
+from langchain.chains.conversation.memory import ConversationBufferWindowMemory
+from langchain.chains import RetrievalQA
+from langchain.agents import Tool
+from langchain.agents import initialize_agent
+
+load_dotenv()  # Load environment variables from .env file
+
+openai_api_key = os.getenv("OPENAI_API_KEY")
+
+# Set your OpenAI API key
+openai.api_key = openai_api_key
+
+env_vars = dotenv_values('.env')
+PINECONE_API_KEY = env_vars['PINECONE_KEY']
+PINECONE_ENV = env_vars['PINECONE_ENVIRON']
+#PINECONE_API_KEY = getpass.getpass("Pinecone API Key:")
+# initialize pinecone
+pinecone.init(
+    api_key=PINECONE_API_KEY,  # find at app.pinecone.io
+    environment=PINECONE_ENV,  # next to api key in console
+)
+# chat completion llm
+llm = ChatOpenAI(
+    openai_api_key=openai_api_key,
+    model_name='gpt-3.5-turbo',
+    temperature=0.0
+)
+# conversational memory
+conversational_memory = ConversationBufferWindowMemory(
+    memory_key='chat_history',
+    k=5,
+    return_messages=True
+)
+
+text_field = "text"
+index_name = "complication"
+# switch back to normal index for langchain
+index = pinecone.Index(index_name)
+embed = OpenAIEmbeddings()
+
+vectorstore = Pinecone(
+    index, embed.embed_query, text_field
+)
+# retrieval qa chain
+qa = RetrievalQA.from_chain_type(
+    llm=llm,
+    chain_type="stuff",
+    retriever=vectorstore.as_retriever()
+)
+
+tools = [
+    Tool(
+        name='Knowledge Base',
+        func=qa.run,
+        description=(
+            'use this tool when answering general knowledge queries to get '
+            'more information about the topic'
+        )
+    )
+]
+
+agent = initialize_agent(
+    agent='chat-conversational-react-description',
+    tools=tools,
+    llm=llm,
+    verbose=True,
+    max_iterations=3,
+    early_stopping_method='generate',
+    memory=conversational_memory
+)
+
 app = Flask(__name__)
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    if request.method == 'POST':
-        # Handle the form submission
-        selected_option = request.form['option']
-        # Do something with the selected option
-        
-    # Render the HTML template
+conversation_history = []
+
+@app.route('/')
+def home():
     return render_template('index.html')
 
+@app.route('/chat', methods=['POST'])
+def chat():
+    #pdb.set_trace()
+    message = request.form['message']
+    #print(request.form)  # Add this line to print the form data
+
+    option = request.form['select-option']
+
+    conversation_history = []
+    # Append user message to conversation history
+    conversation_history.append({'role': 'user', 'content': message})
+
+    # Get response from language model
+    response = generate_response(message, option)
+
+    # Append model response to conversation history
+    conversation_history.append({'role': 'model', 'content': response})
+
+    return jsonify({'message': response})
+
+def answer_by_context(question, context):
+    response_a = openai.Completion.create(
+            prompt=f"Answer the question based on the context below, \
+                  and if the question can't be answered based on the context, \
+                      say \"I don't know\"\n\nContext: \
+                          {context}\n\n---\n\nQuestion: {question}\nAnswer:",
+            temperature=0,
+            max_tokens=150,
+            top_p=1,
+            frequency_penalty=0,
+            presence_penalty=0,
+            stop=None,
+            model="text-davinci-003",
+        )
+    return response_a["choices"][0]["text"].strip()
+
+def response_from_pinecone_index(input_text):
+    #input_file = "cdc_diabetes_text_2.txt"
+    embeddings = OpenAIEmbeddings()
+    index_name = "complication"
+    #docsearch = Pinecone.from_documents(docs, embeddings, index_name=index_name)
+    # if you already have an index, you can load it like this
+    docsearch = Pinecone.from_existing_index(index_name, embeddings)
+    #question = "What is the symptoms of diabetes?"
+    docs = docsearch.similarity_search(input_text, k=3)
+    print(docs[0].page_content)
+    context = docs[0].page_content
+    answer = answer_by_context(input_text,context)
+    return answer
+
+def generate_response(message, option):
+    # Combine conversation history and current message
+    conversation = [{'role': item['role'], 'content': item['content']} for item in conversation_history]
+    conversation.append({'role': 'user', 'content': message})
+
+    # Convert conversation to OpenAI GPT-3 input format
+    input_text = '\n'.join([f"{item['role']}: {item['content']}" for item in conversation])
+
+    # Call the language model API
+    # Debug: Print the input text
+    #print("Input Text:", input_text)
+    # Start the debugger
+    #pdb.set_trace()
+    if option == "chatgpt":
+        #response_b = openai.Completion.create(
+        #    model='gpt-3.5-turbo',#engine='text-davinci-003',
+        #    prompt=input_text,
+        #    max_tokens=50
+        #)
+
+        completion = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {"role": "system", "content": "You are a helpful assistant."},
+                {"role": "user", "content": input_text}
+                ]
+        )
+        #pdb.set_trace()
+        #print(completion)
+        reply = completion.choices[0].message['content']
+        # Extract the model's reply from the response
+        #reply = response_b.choices[0].text.strip()
+        if not reply.startswith("From ChatGPT"):
+            reply = "From ChatGPT: " + reply
+        return reply
+    else:
+        result = agent(input_text)
+        reply2 = result['output']
+        #reply2 = response_from_pinecone_index(input_text)
+        if not reply2.startswith("From Diabetes Content"):
+            reply2 = "From Diabetes Content: " + reply2
+        #print(response)
+        return reply2
+    #return response
+
+if __name__ == '__main__':
+    app.run(debug=True)
