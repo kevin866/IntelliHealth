17c2e9400042d2dd71d1ff454f750df43897c633
kevin886 <k.wang1502483@gmail.com>
2023-07-11 14:29:37 -0700

    integration

--- IntelliHealthApp/IntelliHealthFlask/app.py
+++ IntelliHealthApp/IntelliHealthFlask/app.py
@@ -1,10 +1,9 @@
 import numpy as np
 from flask import Flask, request, json
 from flask_cors import CORS, cross_origin
-
 from service.get_gpt_service import GptService
 from service.get_model_service import ModelService
-
+import langchain_response
 app = Flask(__name__)
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
@@ -14,16 +13,19 @@ app.config['CORS_HEADERS'] = 'Content-Type'
 def health():  # put application's code here
     return 'Status: Healthy, Version: 1.0'
 
-
 @app.route('/api/v1/chat/<chat_content>')
 @cross_origin()
 def chat(chat_content):
+    """response = langchain_response.chatbot(chat_content)
+    print(response)
+    return response
+"""
     cur_service = GptService()
     response_content = cur_service.get_gpt_response(chat_content)
-    # print(type(response_content))
-    # print(response_content["choices"][0])
+    print(type(response_content))
+    print(response_content["choices"][0])
     return response_content["choices"][0]
-
+    
 
 @app.route('/api/v1/model/prediction', methods=['POST'])
 @cross_origin()
@@ -31,18 +33,22 @@ def predict():
     content_type = request.headers.get('Content-Type')
     if content_type == 'application/json':
         json_input = request.json
-        print("model request inputs: ", json_input)
+        #print("model request inputs: ", json_input)
         cur_model = ModelService()
-        print(type(json_input))
+        #print(type(json_input))
         body = json.loads(json_input['body'])
-        print(type(body))
+        #print(type(body))
         matrix = body["data"]
-        print(matrix)
+        #print(matrix)
         input_data = np.array(matrix)
         # print(input_data)
         # print(input_data.shape)
         prediction = cur_model.predict(input_data)
-        print("prediction: ", prediction)
+        if prediction == 1.0:
+            prediction = 'high chances of diabetes'
+        else:
+            prediction = 'no diabetes'
+        #print("prediction: ", prediction)
     else:
         return 'Content-Type not supported!', 400
 
